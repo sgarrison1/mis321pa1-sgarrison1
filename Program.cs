0f472835878ed0803ba17080524c83eb77bc5648
Your Name <you@example.com>
2022-01-21 10:14:25 -0600

    pa1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321pa1_sgarrison1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
